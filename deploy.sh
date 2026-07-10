6ae2b6dda1f4e50686aa54e77887f973912938a0
moun-river <89449660+moun-river@users.noreply.github.com>
2026-07-10 14:04:51 +0900

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+//기존 실행 중인 자바 프로세스 종료
+pid=$(pgrep -f 'my-app-1.0.jar')
+if [ -n "$pid" ]; then
+    kill -9 $pid
+fi
+
+// 새 JAR 실행 (백그라운드)
+nohup java -jar /app/my-app-1.0.jar > /app/app.log 2>&1 &
