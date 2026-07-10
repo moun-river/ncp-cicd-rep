faeb17f13c3d3abbbbca4b96b5badd5a2e751c21
moun-river <89449660+moun-river@users.noreply.github.com>
2026-07-10 16:58:17 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-//기존 실행 중인 자바 프로세스 종료
+# 기존 실행 중인 자바 프로세스 종료
 pid=$(pgrep -f 'my-app-1.0.jar')
 if [ -n "$pid" ]; then
     kill -9 $pid
 fi
 
-// 새 JAR 실행 (백그라운드)
+# 새 JAR 실행 (백그라운드)
 nohup java -jar /app/target/my-app-1.0.jar > /app/logs/app.log 2>&1 &
